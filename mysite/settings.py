e62851cd8c6f5e955bbc77038bbecab2125b6847
KevinCastPer <kecasper@gmail.com>
2018-12-02 14:41:33 +0100

    Autenticar de usuario para poder crear o modificar .... un Post, y boton de login y logout.

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, "static"),
+)
 
 # Application definition
 
@@ -119,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, "static"),
+)
